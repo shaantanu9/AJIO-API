a9040e0c736849a0d7db7b570af71b30c76ab02a
shaantanu9 <shantanubombatkar2@gmail.com>
2022-09-28 21:04:10 +0530

    Added Girls Section

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const data = require('./db.json');
+const data = require("./AJIO.json");
 
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3300;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => console.log(`JSON Server is running on port ${port}`));
+server.listen(port, () =>
+  console.log(`JSON Server is running on port ${port}`)
+);
